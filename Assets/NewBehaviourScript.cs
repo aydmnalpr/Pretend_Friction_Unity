c39c4b828da2dce16a53dba997a2e435c1adba20
Alper Ayduman <aydumanalper@gmail.com>
2021-01-10 20:16:17 +0300

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector3(5,0,0);
+    }
+}
---
+++ Assets/PretendFriction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PretendFriction : MonoBehaviour
+{
+    private Rigidbody myBody;
+    private Transform myTransform;
+    private float myMass;
+    private float slideSpeed;
+    private Vector3 velo;
+    private Vector3 flatVelo;
+    private Vector3 myRight;
+    private Vector3 _vec3;
+
+    public float theGrip = 100f;
+
+    private void Start()
+    {
+        myBody = GetComponent<Rigidbody>();
+        myMass = myBody.mass;
+        myTransform = transform;
+    }
+
+    private void FixedUpdate()
+    {
+        myRight = myTransform.right;
+
+        velo = myBody.velocity;
+        flatVelo.x = velo.x;
+        flatVelo.y = 0;
+        flatVelo.z = velo.z;
+
+        slideSpeed = Vector3.Dot(myRight, flatVelo);
+
+        _vec3 = myRight * (-slideSpeed * myMass * theGrip);
+        
+        myBody.AddForce(_vec3 * Time.deltaTime);
+    }
+}
